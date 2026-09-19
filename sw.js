82f9c8b52d038f72defefd7de25a3782c6b30425
zjchzbb6fp-del <zjchzbb6fp@privaterelay.appleid.com>
2026-09-19 14:09:36 +0500

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,5 @@
+const CACHE='family-flight-3d-v1';
+const FILES=['./','./index.html','./manifest.webmanifest','./three.min.js','./icon-180.png','./icon-192.png','./icon-512.png'];
+self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)).then(()=>self.skipWaiting())));
+self.addEventListener('activate',event=>event.waitUntil(self.clients.claim()));
+self.addEventListener('fetch',event=>{if(event.request.method!=='GET')return;event.respondWith(caches.match(event.request).then(cached=>cached||fetch(event.request).then(response=>{const copy=response.clone();caches.open(CACHE).then(c=>c.put(event.request,copy));return response}).catch(()=>caches.match('./index.html'))));});
